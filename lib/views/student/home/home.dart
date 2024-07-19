042bd45fbe3750dd6ec61080a7ddd9f11965d039
Brick Sigma <bricksigma176@gmail.com>
2024-07-19 22:19:08 +0300

    Added home screen to tutor page

--- lib/views/student/home/home.dart
+++ lib/views/student/home/home.dart
@@ -1,4 +1,7 @@
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
+import 'package:tutoring_app/controllers/main_controller.dart';
+import 'package:tutoring_app/controllers/timetable_controller.dart';
 import 'package:tutoring_app/views/student/home/search.dart';
 import 'package:tutoring_app/views/student/home/settings.dart';
 
@@ -11,9 +14,43 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   final TextEditingController search = TextEditingController();
+  final int currentDay = DateTime.now().weekday - 1;
+  List<List<Map<String, String>>> timetable = [];
+
+  Future<int> _loadTimetable() async {
+    DatabaseReference ref = FirebaseDatabase.instance
+        .ref("users/${MainController.instance.user.uid}/timetable");
+    DataSnapshot snapshot = await ref.get();
+    if (snapshot.exists) {
+      timetable = (snapshot.value as List)
+          .map(
+            (e) => e = (e as List)
+                .map(
+                  (e) => e = Map<String, String>.from(e as Map),
+                )
+                .toList(),
+          )
+          .toList();
+    }
+    if (timetable.isEmpty) {
+      for (int i = 0; i < 7; i++) {
+        timetable.add([
+          nullTimeBlock,
+        ]);
+      }
+    }
+    await ref.set(timetable);
+
+    return 0;
+  }
 
   @override
   Widget build(BuildContext context) {
+    Future<int> loadTimetable = Future.delayed(
+      Duration.zero,
+      () => _loadTimetable(),
+    );
+
     return Scaffold(
       appBar: AppBar(
         title: SizedBox(
@@ -36,19 +73,64 @@ class _HomeState extends State<Home> {
           ),
         ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(12),
-        child: ListenableBuilder(
-          listenable: search,
-          builder: (context, child) {
-            return (search.text.isEmpty)
-                ? const Text("Content")
-                : SearchResults(
-                    search.text,
-                    key: UniqueKey(),
-                  );
-          },
-        ),
+      body: FutureBuilder(
+          future: loadTimetable,
+          builder: (context, snapshot) {
+            Widget child;
+
+            if (snapshot.hasData) {
+              child = Padding(
+                padding: const EdgeInsets.all(12),
+                child: ListenableBuilder(
+                  listenable: search,
+                  builder: (context, child) {
+                    return (search.text.isEmpty)
+                        ? ListView(
+                            children: [
+                              for (Map<String, String> t
+                                  in timetable[currentDay])
+                                if (t["uid"]!.isNotEmpty) TimeBlockCard(t),
+                            ],
+                          )
+                        : SearchResults(
+                            search.text,
+                            key: UniqueKey(),
+                          );
+                  },
+                ),
+              );
+            } else if (snapshot.hasError) {
+              child = const Text("Error!");
+            } else {
+              child = const Center(
+                child: SizedBox(
+                  width: 60,
+                  height: 60,
+                  child: CircularProgressIndicator(),
+                ),
+              );
+            }
+
+            return child;
+          }),
+    );
+  }
+}
+
+class TimeBlockCard extends StatelessWidget {
+  const TimeBlockCard(this.timeblock, {super.key});
+
+  final Map<String, String> timeblock;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: Theme.of(context).colorScheme.secondaryContainer,
+      child: Column(
+        children: [
+          Text("Tutor: ${timeblock["name"]}"),
+          Text(timeblock["start"]!),
+        ],
       ),
     );
   }
--- lib/views/tutor/home/home.dart
+++ lib/views/tutor/home/home.dart
@@ -1,5 +1,7 @@
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:tutoring_app/controllers/main_controller.dart';
+import 'package:tutoring_app/controllers/timetable_controller.dart';
 import 'package:tutoring_app/views/tutor/home/notifications.dart';
 import 'package:tutoring_app/views/tutor/home/settings.dart';
 
@@ -12,11 +14,46 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   final TextEditingController search = TextEditingController();
+  final int currentDay = DateTime.now().weekday - 1;
+  List<List<Map<String, String>>> timetable = [];
+
+  Future<int> _loadTimetable() async {
+    DatabaseReference ref = FirebaseDatabase.instance
+        .ref("users/${MainController.instance.user.uid}/timetable");
+    DataSnapshot snapshot = await ref.get();
+    if (snapshot.exists) {
+      timetable = (snapshot.value as List)
+          .map(
+            (e) => e = (e as List)
+                .map(
+                  (e) => e = Map<String, String>.from(e as Map),
+                )
+                .toList(),
+          )
+          .toList();
+    }
+    if (timetable.isEmpty) {
+      for (int i = 0; i < 7; i++) {
+        timetable.add([
+          nullTimeBlock,
+        ]);
+      }
+    }
+    await ref.set(timetable);
+
+    return 0;
+  }
 
   @override
   Widget build(BuildContext context) {
+    Future<int> loadTimetable = Future.delayed(
+      Duration.zero,
+      () => _loadTimetable(),
+    );
+
     return Scaffold(
       appBar: AppBar(
+        title: const Text("Home"),
         actions: [
           IconButton(
             onPressed: () => Navigator.push(
@@ -41,7 +78,54 @@ class _HomeState extends State<Home> {
           ),
         ],
       ),
-      body: const Placeholder(),
+      body: FutureBuilder(
+          future: loadTimetable,
+          builder: (context, snapshot) {
+            Widget child;
+
+            if (snapshot.hasData) {
+              child = Padding(
+                padding: const EdgeInsets.all(12),
+                child: ListView(
+                  children: [
+                    for (Map<String, String> t in timetable[currentDay])
+                      if (t["uid"]!.isNotEmpty) TimeBlockCard(t),
+                  ],
+                ),
+              );
+            } else if (snapshot.hasError) {
+              child = const Text("Error!");
+            } else {
+              child = const Center(
+                child: SizedBox(
+                  width: 60,
+                  height: 60,
+                  child: CircularProgressIndicator(),
+                ),
+              );
+            }
+
+            return child;
+          }),
+    );
+  }
+}
+
+class TimeBlockCard extends StatelessWidget {
+  const TimeBlockCard(this.timeblock, {super.key});
+
+  final Map<String, String> timeblock;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: Theme.of(context).colorScheme.secondaryContainer,
+      child: Column(
+        children: [
+          Text("Student: ${timeblock["name"]}"),
+          Text(timeblock["start"]!),
+        ],
+      ),
     );
   }
 }
